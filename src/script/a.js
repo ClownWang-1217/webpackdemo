70b711fd85c55778acf11ed6007780559ba3e5af
wtttxy <wt449127919@gmail.com>
2019-09-06 16:06:06 +0800

    add

--- src/script/a.js
+++ src/script/a.js
@@ -1,5 +1,67 @@
+class G
+{
+    constructor()
+    {
+        this.a = 1;
+        this.b = 2;
+    }
+
+    con() {
+        console.log(this.a);
+    }
+
+}
 function a()
 {
     alert("this is a");
 }
 
+
+
+var A = {
+    Bar:function()
+    {
+        console.log(2);
+
+    }
+}
+
+function Bar(x = 2,y = x)
+{
+    console.log(x);
+}
+
+
+module.exports= function ()
+{
+    
+    var g = new G();
+    G.prototype.gg = ()=>{console.log('dfdf')};
+    g.con();
+    g.gg();
+    var promis1 = new Promise(function(resolve,reject){
+
+        setTimeout(() => {
+            resolve("resolve");
+        }, 2000);
+
+       
+    })
+    var promis2 = new Promise(function(resolve,reject){
+
+        setTimeout(() => {
+            reject(promis1);
+        }, 1000);
+
+       
+    })
+    
+    promis2.then(reject=>{
+
+    }).catch(error => {
+        console.log(error);
+    });
+
+    
+
+}
